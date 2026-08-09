d2506fb2d622749b120bb76336aee4ec15e5f334
Mouni0176 <24331a0539@mvgrce.edu.in>
2026-08-09 20:39:50 +0530

    ✅ MAJORELEM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-09

--- codechef/medium/MAJORELEM/solution.java
+++ codechef/medium/MAJORELEM/solution.java
@@ -1,25 +1,12 @@
 class Solution {
     public int majorityElement(int[] arr) {
-        
-        
-      int max = Integer.MIN_VALUE;
-        int n = arr.length;
-        for(int i =0;i<arr.length;i++){
-            int count = 1 ;
-            max = arr[i];
-            for(int j=i+1; j<arr.length;j++){
-                
-            
-            if(arr[j]==max){ 
-                
-                count++;
-                
-            }
-            if(count>=n/2){
-                return max;
-            }
+    int count =0;
+    int max=0;
+    for(int i : arr){
+        if(count ==0){
+            max = i;
         }
-        
+        count += ((i == max) ? 1 : -1);
     }
     return max;
 }
