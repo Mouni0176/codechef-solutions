f1f7c19521cd7b5a9c43e01dd33f011452e18682
Mouni0176 <24331a0539@mvgrce.edu.in>
2026-08-09 20:20:39 +0530

    ✅ MAJORELEM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-09

--- codechef/medium/MAJORELEM/solution.java
+++ codechef/medium/MAJORELEM/solution.java
@@ -5,10 +5,18 @@ class Solution {
       int max = Integer.MIN_VALUE;
         int n = arr.length;
         for(int i =0;i<arr.length;i++){
-            
-            if(arr[i]>max){ 
+            int count = 0 ;
+            for(int j=0; j<arr.length;j++){
                 max = arr[i];
+            
+            if(arr[j]==max){ 
+                max = arr[i];
+                count++;
                 
+            }
+            if(count>=n/2){
+                return max;
+            }
         }
         
     }
