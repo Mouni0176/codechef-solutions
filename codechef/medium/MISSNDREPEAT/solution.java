5bb2d8847756c68e3b61d78e7991d6726f488cf2
Mouni0176 <24331a0539@mvgrce.edu.in>
2026-08-29 19:38:22 +0530

    ✅ MISSNDREPEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-29

--- codechef/medium/MISSNDREPEAT/solution.java
+++ codechef/medium/MISSNDREPEAT/solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] findRepeatingAndMissing(int[] arr) {
         int n= (arr.length*(arr.length+1))/2;
-        // write your code here
+        
         int found = 0;
-        int ff=0;
+        
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++)
             {
