9b143cd79cd57b228ab9b19101b2984877d281aa
Mouni0176 <24331a0539@mvgrce.edu.in>
2026-08-30 16:08:44 +0530

    ✅ DSACPR53 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-30

--- codechef/medium/DSACPR53/solution.java
+++ codechef/medium/DSACPR53/solution.java
@@ -6,23 +6,25 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int [][] arr = new int [m][n];
+		for(int i = 0;i<m ;i++){
+		    st = new StringTokenizer(br.readLine());
+		    for(int j=0;j<n;j++){
+		        arr[i][j] = Integer.parseInt(st.nextToken());
+		    }
+		}
+		StringBuffer sb = new StringBuffer();
+		for(int i=0;i<m;i++){
+		    for(int j=0;j<n;j++){
+		        sb.append(arr[i][j]).append(" ");
+		    }
 		
-		Scanner scanner = new Scanner(System.in);
-		int m = scanner.nextInt();
-		int n= scanner.nextInt();
-		int [][] arr = new int[m][n];
-		for(int i=0;i<m;i++){
-		    for(int j=0;j<n;j++){
-		        arr[i][j]=scanner.nextInt();
-		    }
+		    sb.append('\n');
 		}
-		for(int i=0;i<m;i++){
-		    for(int j=0;j<n;j++){
-		        System.out.print(arr[i][j]+" ");
-		    }
-		    System.out.println();
-		}
-    
+		System.out.println(sb);
 	}
 }
