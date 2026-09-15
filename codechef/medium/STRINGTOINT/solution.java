7dc64841067285516f3998f62086974bedffab31
Mouni0176 <24331a0539@mvgrce.edu.in>
2026-09-15 20:10:09 +0530

    ✅ STRINGTOINT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/STRINGTOINT/solution.java
@@ -0,0 +1,30 @@
+public static int myAtoi(String s) {
+    if(s==null||s.length()==0){
+        return 0;
+    }
+    int i=0,n=s.length();
+    while(i<n && n==' ' ){
+        i++;
+    }
+    if(i==n) return 0;
+    int sign =1;
+    if(s.charAt(i)=='-'){
+        sign=-1;
+        i++;
+    }
+    else if(s.charAt(i)=='+'){
+        sign=1;
+        i++;
+    }
+    long num =0;
+    while(i<n && Character.isDigit(s.charAt(i))){
+        num =num*10+(s.charAt(i)-'0');
+        if((sign ==1) && (num>Integer.MAX_VALUE))
+            return Integer.MAX_VALUE;
+        if((sign==-1) && (num<Integer.MIN_VALUE))
+            return Integer.MIN_VALUE;
+            
+        i++;
+    }
+    return (int)(sign*num);
+}
