e45e6d74dc13ffd3d212f3c196918ad14bbc1e8e
Mouni0176 <24331a0539@mvgrce.edu.in>
2026-09-17 20:30:18 +0530

    ✅ DSACPR63 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-17

--- codechef/medium/DSACPR63/solution.java
+++ codechef/medium/DSACPR63/solution.java
@@ -11,16 +11,13 @@ class Codechef
 		
 		char letter = scanner.next().charAt(0);
 		
-		int found =-1;
 		for(int i=0;i<word.length();i++){
 		    if(word.charAt(i) == letter){
 		        System.out.println(i);
-		        found =0;
 		    }
-		   
-		}
-		if(found ==-1){
-		    System.out.println("-1");
+		    else{
+		        System.out.println("-1");
+		    }
 		}
 	}
 }
