4de00ab8042b0c1f699f1fcb8d6e3f5045ec24df
Mouni0176 <24331a0539@mvgrce.edu.in>
2026-09-17 20:23:23 +0530

    ✅ DSACPR63 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ codechef/medium/DSACPR63/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+		String word = scanner.nextLine();
+		
+		char letter = scanner.next().charAt(0);
+		
+		for(int i=0;i<word.length();i++){
+		    if(word.charAt(i) == letter){
+		        System.out.println(i);
+		    }
+		}
+	}
+}
