0154ee96bbbfdf21f94f46174becfe2f90c5ff1a
Mouni0176 <24331a0539@mvgrce.edu.in>
2026-09-22 21:26:06 +0530

    ✅ DSACPR124 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-22

--- codechef/medium/DSACPR124/solution.java
+++ codechef/medium/DSACPR124/solution.java
@@ -1,40 +1,27 @@
- class LinkedList {
-    Node head;
-
-    public void insertAtIndex(int index, int value) {
-        Node newNode = new Node(value);
-
-        if (index == 0) {
-            newNode.next = head;
-            if (head != null) {
-                head.prev = newNode;
-            }
-            head = newNode;
-        } else {
-            Node iter = head;
-            for (int i = 0; i < index - 1; i++) {
-                iter = iter.next;
-            }
-
-            Node A = iter;
-            Node B = iter.next;
-
-            A.next = newNode;
-            if (B != null) {
-                B.prev = newNode;
-            }
-
-            newNode.next = B;
-            newNode.prev = A;
-        }
+void deleteNode(int value) {
+    Node targetNode = head;
+    while (targetNode != null && targetNode.value != value) {
+        targetNode = targetNode.next;
     }
 
-    public void printValues() {
-        Node current = head;
-        while (current != null) {
-            System.out.print(current.value + " ");
-            current = current.next;
-        }
-        System.out.println();
+    if (targetNode == null)
+        return;
+
+    // Update A and B
+    Node A = targetNode.prev;
+    Node B = targetNode.next;
+
+    // A could be null if target is head
+    if (A != null) {
+        A.next = B;
+    }
+
+    // B could be null if target is tail
+    if (B != null) {
+        B.prev = A;
+    }
+
+    if (targetNode == head) {
+        head = B;
     }
 }
